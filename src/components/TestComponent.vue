f131d144b7243d01b774a171a8e31d64616dcc64
Vitaliy <VYugay@szhuldyz.kz>
2025-02-08 23:38:06 +0500

    created project

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+  },
+}
+</script> -->
+<script setup>
+defineProps({
+  title: String,
+})
+</script>
+<template>
+  <div class="border p-4">
+    {{ title }}
+  </div>
+</template>
+<style scoped></style>
